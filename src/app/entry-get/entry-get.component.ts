50b586895b1b8efb6f1cdfffd6d7c3410c6b9d0d
rubyGoldberg <z5162022@cse.unsw.edu.au>
2019-01-15 22:45:20 +1100

    deleted animations

--- src/app/entry-get/entry-get.component.ts
+++ src/app/entry-get/entry-get.component.ts
@@ -21,16 +21,12 @@ import { trigger, state, style, animate, transition, query, stagger, keyframes }
                 style({opacity: .8, transform: 'translateY(35px)',  offset: 0.3}),
                 style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
               ]))]), {optional: true})
-
-          ])
-      ]), 
-
-    trigger('delete',[
-      transition(':leave', [
-          animate('1s ease-out', style({ opacity: 0, transform: 'translateX(-50%) translateY(-50%) scale(.7)' }))
-        ]),
+          ]), 
+        transition(':leave', [
+          style({ transform: 'translateX(0%)' }),
+          animate('0.5s ease-in-out', style({ transform: 'translateX(-100%)' }))
+          ],)
       ])
-
   ]
 })
 export class EntryGetComponent implements OnInit {
@@ -55,9 +51,8 @@ export class EntryGetComponent implements OnInit {
         const table = document.getElementById("tablebody");
         let deletedEntry = document.getElementById(id);
         console.log("this is the entry we are going to remove from the dom: ", deletedEntry);
-        table.removeChild(deletedEntry);
-        
-
+        //table.removeChild(deletedEntry);
+        deletedEntry.classList.add("animate");
       })
   }
 
